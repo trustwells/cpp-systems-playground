54885cdef1a998d41998a266e64610485b1bc4a7
trustwells <trust.wells@gmail.com>
2025-12-09 16:01:39 -0600

    idk

--- type-deduction/auto.cpp
+++ type-deduction/auto.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
 
 
-
-
-
 int main() {
 
 
-	auto x1 = 27;
-	auto x2(27);
-	auto x3 = { 27 };
-	auto x4{ 27 };
-
-	// x1 and x2 are deduced to be ints
-	// 
-	// but x3 and x4 are deduced to be std::initializer_list because of curly brace initialization
 	
 
+	
 
 	return 0;
 
