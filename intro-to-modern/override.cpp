220af4aa39959ab87ff2ca4fecf98a9cc4d43348
trustwells <trust.wells@gmail.com>
2026-01-23 17:51:49 -0600

    update

---
+++ intro-to-modern/override.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+class Base {
+
+//    public:
+  //      virtual void doWork();
+};
+
+
+
+
+
+int main() {
+
+
+    std::cout << "size of Base: " << sizeof(Base) << '\n';    
+
+    return 0;
+
+
+
+}
